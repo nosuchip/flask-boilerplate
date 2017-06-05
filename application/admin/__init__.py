b8e0918b011debb37f7ac95060ab3ef259666fcc
Alex Popov <nosuchip@gmail.com>
2017-06-05 21:32:39 +0700

    Cleanup repo

--- application/admin/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from application.config.app import APP_NAME
-from flask.ext.admin import Admin
-from application.admin.views import add_admin_views
-
-def init_admin(app):
-    admin = Admin(app, name=APP_NAME)
-
-    add_admin_views(admin)
--- application/core/filters.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from flask import current_app
-
-strip = lambda s: s.strip() if s is not None else None
-lower = lambda s: s.lower() if s is not None else None
-
-def app_config_item(name):
-    return current_app.config[name]
